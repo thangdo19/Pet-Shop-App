a2dee9c72c2a1493e2374c6083de5b5b3913c424
Thang Do <18thangd@gmail.com>
2020-07-07 10:52:40 +0700

    Add async methods for handling CRUD

--- src/com/petshop/Main.java
+++ src/com/petshop/Main.java
@@ -2,16 +2,34 @@ package com.petshop;
 
 import com.petshop.bll.Application;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    var app = new Application();
+    var app = new Application(4);
 
+//    app.updatePetAsync();
     app.run();
 
-    try {
-      Thread.sleep(10_000);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
+    while(true) {
+      Scanner reader = new Scanner(System.in);
+      var read = reader.nextLine();
+
+      if (read.equals("stop")) {
+        app.shutdownExecutor();
+        break;
+      }
+
+      if (read.equals("remove")) {
+        var id = reader.nextLine();
+        app.removePetAsync(id);
+      }
+
+      if (read.equals("show")) {
+        app.showAllPetsAsync();
+      }
+
+      System.out.println(read);
     }
   }
 }
--- src/com/petshop/bll/Application.java
+++ src/com/petshop/bll/Application.java
@@ -1,20 +1,74 @@
 package com.petshop.bll;
 
+import com.petshop.bll.interfaces.PetGettable;
 import com.petshop.bll.interfaces.PetModdable;
+import com.petshop.bll.logic.PetGetter;
 import com.petshop.bll.logic.PetModder;
 import com.petshop.dto.Pet;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Application {
-  public Application() {}
+  private ExecutorService executor;
+
+  public Application(int nThreads) {
+    // create 4 worker threads for this executor
+    executor = Executors.newFixedThreadPool(nThreads);
+  }
 
   public void run() {
     var pet = new Pet("DOG004", "Ha Noi", "Dog", "Sol", "Male", 2);
     var newPet = new Pet("DOG004", "TP Ho Chi Minh", "Dog", "Sol", "Male", 2);
 
     PetModdable petModdable = PetModder.getInstance();
-    System.out.println(petModdable.updatePet(pet.getId(), newPet));
+//    System.out.println(petModdable.updatePet(pet.getId(), newPet));
 //    System.out.println(petModdable.deletePet(pet.getId()));
-//    System.out.println(petModdable.addPet(pet));
+    System.out.println(petModdable.addPet(pet));
+  }
+
+  public void showAllPetsAsync() {
+    CompletableFuture.runAsync(() -> {
+        PetGettable petGettable = PetGetter.getInstance();
+        System.out.println(petGettable.getAllPets());
+      }
+      , this.executor
+    );
+  }
+
+  public void updatePetAsync() {
+    CompletableFuture.runAsync(() -> {
+        var pet = new Pet("DOG004", "Ha Noi", "Dog", "Sol", "Male", 2);
+        var newPet = new Pet("DOG004", "TP Ho Chi Minh", "Dog", "Sol", "Male", 2);
+
+        PetModdable petModdable = PetModder.getInstance();
+        System.out.println(petModdable.updatePet(pet.getId(), newPet));
+      }
+      , this.executor
+    );
+  }
+
+  public void removePetAsync(String id) {
+    CompletableFuture
+      .supplyAsync(() -> {
+          PetModdable petModdable = PetModder.getInstance();
+          return petModdable.deletePet(id);
+        }
+        , this.executor
+      )
+      .thenAccept(isRemoved -> {
+        if (isRemoved)
+          System.out.println("Removing Successfully");
+        else
+          System.out.println("Removing Unsuccessfully");
+      });
+  }
+
+  public void shutdownExecutor() {
+    if (executor != null)
+      executor.shutdown();
   }
 }
 
